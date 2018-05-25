b8e025c29207863ade060b23a4f4d643550e6cb5
Ilyas Almakaev <i.almakaev@gmail.com>
2018-05-25 15:44:03 +0700

    add toggle

--- notes/containers/notes/Notes.js
+++ notes/containers/notes/Notes.js
@@ -3,12 +3,18 @@ import {
     Button,
     View,
     FlatList,
-    Text
+    Text,
+    Switch
   } from 'react-native';
 import { styles } from '../../Styles';
   
 
 export default class Notes extends Component {
+
+  constructor() {
+    super();
+    this.state = { done: true };
+  }
 
   componentWillMount() {
     this.props.navigation.setParams({
@@ -20,14 +26,42 @@ export default class Notes extends Component {
     alert('add note')
   }
 
-  render() {
+  onDeleteNote() {
+    alert('delete note')
+  }
 
+  onDone = (value) => {
+    this.setState({ done: value })
+ }
+
+  render() {
+    
+    let notes=[{key: 'a'}, {key: 'b'}];
+    let notesTemplate;
+
+    if (notes.length > 0) {
+      notesTemplate = <FlatList
+        data = {notes}
+        renderItem={({item}) => (
+          <View>
+            <Text>{"item.title"}</Text>
+            <Text>{"item.body"}</Text>
+            <Switch 
+              onValueChange={ this.onDone } 
+              value={ this.state.done } 
+            />
+            <Button
+              onPress={this.onDeleteNote}
+              title="Delete Note"
+            />
+          </View>)}
+      />
+    } else {
+      notesTemplate = <Text>{'There are no notes'}</Text>
+    }
     return (
 	    <View style={styles.container}>
-        <FlatList
-          data={[{key: 'a'}, {key: 'b'}]}
-          renderItem={({item}) => <Text>{item.key}</Text>}
-        />
+        {notesTemplate}
       </View>
     );
   }
